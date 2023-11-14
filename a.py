fa08784b2de19beb1755e060721b4b376eed8683
Arthur Buzelin <125529624+Buzelin2@users.noreply.github.com>
2023-11-14 19:25:17 -0300

    Update a.py

--- a.py
+++ a.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import geckodriver_autoinstaller
 import os
 
 # Verificar se o geckodriver está instalado e, se não estiver, instalar localmente
